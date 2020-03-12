6718d726751faad72f5d787d703922af0a1a0965
= <=>
2020-03-12 02:19:33 -0700

    started basic js

--- js.js
+++ js.js
@@ -1,74 +1,116 @@
 // CINDY START
 var data = {
-    qNum = 0,
+    qnum: 0,
     questions: [
         {
-            title: "",
+            scenario: "The popular kid in your class starts picking his nose and wiping it onto everyone's desk. Since everyone thinks he's so cool, they do it too.",
+            answers: [
+                "JOIN THEM SO I CAN BE COOL TOO!",
+                "No... I think that's gross, so I’ll pass."
+            ],
+            correctAnswer: "",
+            img: ""
+        },
+        {
+            scenario: "",
+            answer1: "",
+            img: ""
+        },
+        {
+            scenario: "",
             answer1: "",
             answer2: "",
             img: ""
         },
         {
-            title: "",
+            scenario: "",
             answer1: "",
             answer2: "",
             img: ""
         },
         {
-            title: "",
+            scenario: "",
             answer1: "",
             answer2: "",
             img: ""
         },
         {
-            title: "",
+            scenario: "",
             answer1: "",
             answer2: "",
             img: ""
         },
         {
-            title: "",
+            scenario: "",
             answer1: "",
             answer2: "",
             img: ""
         },
         {
-            title: "",
+            scenario: "",
             answer1: "",
             answer2: "",
             img: ""
         },
         {
-            title: "",
+            scenario: "",
             answer1: "",
             answer2: "",
             img: ""
         },
         {
-            title: "",
+            scenario: "",
             answer1: "",
             answer2: "",
             img: ""
         },
         {
-            title: "",
-            answer1: "",
-            answer2: "",
-            img: ""
-        },
-        {
-            title: "",
-            answer1: "",
-            answer2: "",
-            img: ""
-        },
-        {
-            title: "",
+            scenario: "",
             answer1: "",
             answer2: "",
             img: ""
         },
     ]
+}
+
+// ====================  PAGE TRANSITIONS ==================== 
+var page = "home_page";
+// home_page, start_page, question_page, correct_page, incorrect_page, etc.
+
+
+function goStartPage() {
+    document.querySelector("#home-page").style.display = "none";
+    document.querySelector("#lets-start-page").style.display = "flex";
+    page = "start_page"
+}
+
+function startQuiz() {
+    document.querySelector("#lets-start-page").style.display = "none";
+    document.querySelector("#question-page").style.display = "flex";
+    page = "question_page"
+}
+
+// ==================== QUIZ ====================
+
+function leftAnswer() {
+    document.querySelector("#scenario-answer1").style.border = "solid 5px #FAFAFA";
+    document.querySelector("#scenario-answer2").style.border = "0";
+    document.querySelector("#scenario-submit").style.opacity = "1";
+}
+
+function rightAnswer() {
+    document.querySelector("#scenario-answer2").style.border = "solid 5px #FAFAFA";
+    document.querySelector("#scenario-answer1").style.border = "0";
+    document.querySelector("#scenario-submit").style.opacity = "1";
+}
+
+showQuestions();
+
+function showQuestions() {
+    document.querySelector("#scenario-prompt").innerText = data.questions[data.qnum].scenario;
+    document.querySelector("#scenario-answer1").innerText = data.questions[data.qnum].answers[0];
+    document.querySelector("#scenario-answer2").innerText = data.questions[data.qnum].answers[1];
+    data.qnum++;
 }
 
 
