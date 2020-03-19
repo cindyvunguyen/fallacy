67b9f30fd48a681204704e08bfa9b650dde348df
= <=>
2020-03-19 01:08:59 -0700

    made quiz functional

--- js.js
+++ js.js
@@ -10,7 +10,9 @@ var data = {
                 "No... I think that's gross, so I'll pass."
             ],
             correct_answer: "right-answer",
-            img: ""
+            img: "",
+            fallacy: "BANDWAGON FALLACY",
+            explanation: "Listening to what is popular instead of listening to yourself."
         },
         {
             scene: 2,
@@ -20,87 +22,105 @@ var data = {
                 "I think that is enough… They were crying! What a poor person."
             ],
             correct_answer: "left-answer",
-            img: ""
+            img: "",
+            fallacy: "APPEAL TO EMOTION",
+            explanation: "Using people's feelings to convince them or win an argument."
         },
         {
             scene: 3,
             scenario: "Your parents don’t like your music teacher because she did not graduate from the top-rated local university and graduated from a smaller college.",
             answers: [
-                "I agree, teachers should have their degrees from prestigious universities.",
+                "Agree, teachers should graduate from prestigious universities.",
                 "I don't think a prestigious college diploma matters."
             ],
             correct_answer: "right-answer",
-            img: ""
+            img: "",
+            fallacy: "AD HOMINEM",
+            explanation: "Attacking someone instead of listening to what they're saying."
         },
         {
             scene: 4,
             scenario: "Johnny likes to play the piano and would rather do that than play sports. His friends make fun of him, telling him that playing the piano is for nerds.",
             answers: [
-                "He should stop playing the piano and start to play sports, because he'll be a nerd.",
+                "Stop playing the piano because he'll be a nerd.",
                 "Continue to play the piano because he really enjoys it."
             ],
             correct_answer: "right-answer",
-            img: ""
+            img: "",
+            fallacy: "BANDWAGON FALLACY",
+            explanation: "Listening to what is popular instead of listening to yourself."
         },
         {
             scene: 5,
             scenario: "Cindy is against social media because she prefers face-to-face interactions. However, more and more of Cindy's friends have joined social media sites.",
             answers: [
                 "She should stay true to my values of not wanting social media.",
-                "Join all the social medias to fit in!"
+                "Join all the social medias to fit in! Cindy can't be a loser!"
             ],
             correct_answer: "left-answer",
-            img: ""
+            img: "",
+            fallacy: "BANDWAGON FALLACY",
+            explanation: "Listening to what is popular instead of listening to yourself."
         },
         {
             scene: 6,
             scenario: "A Foodbank commercial that shows hungry family members with no food on the dinner table. The commercial proceeds to ask viewers to donate money for the food.",
             answers: [
-                "They look so desperate! They need help, so I need to donate right now!",
-                "The commercial brings awareness, however, donations alone will not solve the problem."
+                "They look so desperate! I need to donate right now!",
+                "The commercial brings awareness. I will donate if I want to."
             ],
             correct_answer: "right-answer",
-            img: ""
+            img: "",
+            fallacy: "APPEAL TO EMOTION",
+            explanation: "Using people's feelings to convince them or win an argument."
         },
         {
             scene: 7,
             scenario: "Victor didn't want to eat his carrots and broccoli. His father told him to think about the poor, starving children in the world who weren't fortunate enough to have any food at all.",
             answers: [
-                "Think of the starving children, Victor's gotta finish it now!",
-                "No, persuading his son through feelings is not a good reason to eat the vegetable against his will."
+                "Think of the starving children, Victor NEEDS to finish it now!",
+                "Victor should eat it if he wants to. He should not be persuaded."
             ],
             correct_answer: "right-answer",
-            img: ""
+            img: "",
+            fallacy: "APPEAL TO EMOTION",
+            explanation: "Using people's feelings to convince them or win an argument."
         },
         {
             scene: 8,
             scenario: "Joy Tribbiani loves eating triple-double hamburgers with extra large french fries. He notices that many of his friends have started eating a low-carb diet and drinking protein shakes.",
             answers: [
-                "If Joy wants a burger, she sohuld eat a burger.",
-                "Join her friends’ low-carb diet, since all my friends are doing it."
+                "If Joy wants a burger, she should eat a burger.",
+                "Immediately join her friends’ fancy low-carb diet, since all her friends are doing it."
             ],
             correct_answer: "left-answer",
-            img: ""
+            img: "",
+            fallacy: "BANDWAGON FALLACY",
+            explanation: "Listening to what is popular instead of listening to yourself."
         },
         {
             scene: 9,
             scenario: "A politician, Andrew argues that his opponent cannot possibly be a good choice for female voters because he married and divorced five times despite his competency in politics.",
             answers: [
                 "Why should I trust someone who failed their marriage FIVE times?!",
-                "Well, I'm not voting for a marriage counselor. I need a competent and honest politician."
+                "I'm not voting for a marriage counselor. I need a competent and honest politician."
             ],
             correct_answer: "right-answer",
-            img: ""
+            img: "",
+            fallacy: "AD HOMINEM",
+            explanation: "Attacking someone instead of listening to what they're saying."
         },
         {
             scene: 10,
             scenario: "Your younger sister has a fever so you and mom are taking her to the clinic.  There are two doctors at the clinic. One is a mother, Dr. Amy, and the other is Dr. Jason who just graduated from medical school from Europe.",
             answers: [
                 "I think my mom is right. I also prefer a mom pediatrician.",
-                "What's wrong with either of them?"
+                "What's wrong with either of them? Both are great."
             ],
             correct_answer: "right-answer",
-            img: ""
+            img: "",
+            fallacy: "AD HOMINEM",
+            explanation: "Attacking someone instead of listening to what they're saying."
         }
     ]
 }
@@ -120,6 +140,7 @@ function startQuiz() {
 // ==================== QUIZ ====================
 
 function showQuestions() {
+    document.querySelector("#scenario-current-number").innerText = data.questions[data.qnum].scene;
     document.querySelector("#scenario-prompt").innerText = data.questions[data.qnum].scenario;
     document.querySelector("#left-answer").innerText = data.questions[data.qnum].answers[0];
     document.querySelector("#right-answer").innerText = data.questions[data.qnum].answers[1];
@@ -141,25 +162,71 @@ function rightAnswer() {
     selected_answer = "right-answer";
 }
 
+// resets buttons, so none are selected on new question
+function resetButtons() {
+    document.querySelector("#right-answer").style.border = "0";
+    document.querySelector("#left-answer").style.border = "0";
+    document.querySelector("#scenario-submit").style.opacity = "0.5";
+}
+
 showQuestions();
 
 function checkAnswer() {
     if (selected_answer === data.questions[data.qnum].correct_answer) {
-        // INSERT CODE HERE TO GO TO CORRECT PAGE.
-        document.querySelector("#scenario-prompt").innerText = "Sick dude.";
-        // qnum++
+        console.log(data.qnum)
+
+        // switches the pages
+        document.querySelector("#correct-incorrect-page").style.display = "flex";
+        document.querySelector("#question-page").style.display = "none";
+
+        //
+        correctPage();
+
+        // inputs the text required
+        document.querySelector("#cip-fallacy").innerText = data.questions[data.qnum].fallacy;
+        document.querySelector("#cip-definition").innerText = data.questions[data.qnum].explanation;
+        data.qnum++;
     } else {
-        // INSERT CODE HERE TO GO TO INCORRECT PAGE.
-        document.querySelector("#scenario-prompt").innerText = "You're wrong."
+        console.log(data.qnum)
+
+        // switches the pages
+        document.querySelector("#correct-incorrect-page").style.display = "flex";
+        document.querySelector("#question-page").style.display = "none";
+
+        // makes the page incorrect
+        incorrectPage();
+        
+        // inputs the text required
+        document.querySelector("#cip-fallacy").innerText = data.questions[data.qnum].fallacy;
+        document.querySelector("#cip-definition").innerText = data.questions[data.qnum].explanation;
+        data.qnum++;
+    }
+}
+
+function nextQuestion() {
+
+    if (data.qnum == 10) {
+        endGame();
+    } else {
+
+        // switches pages
+        document.querySelector("#correct-incorrect-page").style.display = "none";
+        document.querySelector("#question-page").style.display = "flex";
+
+        resetButtons();
+
+        // inserts new question
+        document.querySelector("#scenario-current-number").innerText = data.questions[data.qnum].scene;
+        document.querySelector("#scenario-prompt").innerText = data.questions[data.qnum].scenario;
+        document.querySelector("#left-answer").innerText = data.questions[data.qnum].answers[0];
+        document.querySelector("#right-answer").innerText = data.questions[data.qnum].answers[1];
     }
 }
 
 function endGame() {
-    if (qnum >= 9) {
-        // INSERT CODE TO END SCREEN
-    }
+    document.querySelector("#correct-incorrect-page").style.display = "none";
+    document.querySelector("#results-page").style.display = "flex";
 }
-
 
 // CINDY END
 
@@ -173,6 +240,13 @@ function endGame() {
 
 //            fallacy:"AD HOMINEM",
 //            definition:"Attacking someone instead of listening to what they're saying."
+
+function correctPage() {
+    document.querySelector("#correct-incorrect-page").style.backgroundColor = "#6eb748";
+    document.querySelector("#cip-text1").innerText = "HORRAY!";
+    document.querySelector("#cip-text2").innerText = "You avoided the";
+    document.querySelector("#icon").style.backgroundColor = "olive";
+}
 
 function incorrectPage() {
     document.querySelector("#correct-incorrect-page").style.backgroundColor = "#860000";
@@ -202,34 +276,8 @@ function threeFallacies() {
     document.querySelector("#three-fallacies-page").style.display = "flex";
 }
 
-// var data = {
-//     dnum: 0,
-//     definitions: [
-//         {
-//             fallacy:"Appeal to Emotion is an argument that is made due to the manipulation of emotions, rather than using valid reasoning."
-//         },
-//         {
-//             fallacy:"Ad Hominem is an argument that is built around an emotional appeal, rather than logical appeal."
-//         },
-//     ]
-// }
-
-// function fallacyChange() {
-//     document.querySelector("#tfp-text").innerHTML = data.definitions
-// }
-
 function fallacyChange() {
     document.querySelector("#tfp-text").innerHTML = "<p><b>Appeal to Emotion</b> is an argument that is made due to the manipulation of emotions,<br>rather than using valid reasoning.</p>";
 }
-
-// function fallacyChange(state) {
-//     switch(state) {
-//         case 0:
-//             document.querySelector("#tfp-text").innerHTML = "<p><b>Appeal to Emotion</b> is an argument that is made due to the manipulation of emotions,<br>rather than using valid reasoning.</p>";
-//         case 1:
-//             document.querySelector("#tfp-text").innerHTML = "<p><b>Ad Hominem</b> is an argument that is built around an emotional appeal,<br>rather than logical appeal.</p>";
-//         break;
-//     }
-// }
 
 // CHLOE END
